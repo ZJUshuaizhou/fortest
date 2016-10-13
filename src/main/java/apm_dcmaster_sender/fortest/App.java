ff78d80b26c58901bda9db448cfb80b233cb4c41
zhoushuai <comegogo@yeah.net>
2016-10-13 10:56:44 +0800

    fortest

---
+++ src/main/java/apm_dcmaster_sender/fortest/App.java
@@ -0,0 +1,13 @@
+package apm_dcmaster_sender.fortest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
